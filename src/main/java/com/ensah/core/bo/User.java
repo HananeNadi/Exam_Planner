c0e12ddfb090d4638b1d77ff08bd504dc649dbad
Hamza MOTASSIM <motassimhamza99@gmail.com>
2024-05-27 15:54:31 +0100

    Security Config

--- src/main/java/com/ensah/core/bo/User.java
+++ src/main/java/com/ensah/core/bo/User.java
@@ -1,18 +1,32 @@
 package com.ensah.core.bo;
 
 
-import jakarta.persistence.Entity;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-//@Getter
-//@Setter
-//@Entity
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter @AllArgsConstructor
+@Setter @NoArgsConstructor
+@Entity @Table(name = "users")
 public class User {
 
 
-    private Long idUser;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String username;
+    private String password;
+    private String roles;
 
-
+    public User(String username, String password, String roles) {
+        this.username = username;
+        this.password = password;
+        this.roles = roles;
+    }
 
 }
--- src/main/java/com/ensah/core/config/CorsConfiguration.java
+++ src/main/java/com/ensah/core/config/CorsConfiguration.java
@@ -1,11 +1,13 @@
 package com.ensah.core.config;
 
+import io.micrometer.common.lang.NonNullApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+//@NonNullApi
 public class CorsConfiguration {
     @Bean
     public WebMvcConfigurer corsConfigurer() {
